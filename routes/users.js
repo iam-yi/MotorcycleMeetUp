2805d415bad4f3b1c17e070dfacded107ac3df97
Yi Li <youremail@domain.com>
2022-04-23 09:39:59 -0700

    home nav

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+// const listCtrl = require('../controllers/lists');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', listCtrl.index);
 
 module.exports = router;
